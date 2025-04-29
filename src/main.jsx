6ae7f9ba553ee0017517b4a87a56b6d2f1c315e7
kingdow10123 <80277412+kingdow10123@users.noreply.github.com>
2025-04-29 13:25:45 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FoodRecord from './components/FoodRecord.jsx';
+import './components/FoodRecord.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FoodRecord />
+  </React.StrictMode>
+);
